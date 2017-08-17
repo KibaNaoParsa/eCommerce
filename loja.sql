73c75518208aa75b5406d3d4c9a25bee5587fb2c
Elyas Correa Nogueira <elyasnog2@hotmail.com>
2017-08-17 08:34:19 -0300

    bd

--- loja.sql
+++ loja.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 10-Ago-2017 às 08:41
+-- Generation Time: 17-Ago-2017 às 08:34
 -- Versão do servidor: 5.7.18-0ubuntu0.16.04.1
 -- PHP Version: 7.0.21-1~ubuntu16.04.1+deb.sury.org+1
 
@@ -37,8 +37,38 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `titulo`, `descricao`) VALUES
-(1, 'Chocolates', 'Chocolate'),
-(2, 'Massas', 'Macarrão, Pão, etc...');
+(1, 'massa', 'macarrão'),
+(2, 'chocolate', 'chocolate amargo'),
+(3, 'salgado', 'salgadooo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
+('0ggjktjdrpn96ucdehder3ri3s0vei66', '::1', 1502967860, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936373836303b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('4tdkv1ok6m0b67qh5utrdeps4ke891ch', '127.0.0.1', 1502895090, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323839353039303b),
+('8mi2f4bqu61gio9cbe4d0c5sn0i6occn', '127.0.0.1', 1502895090, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323839353039303b),
+('9vf7agefq20736526ro1k20g814al44q', '::1', 1502966124, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936363132343b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('c16amrkb0rj8q7nhbn12jl5l7r1vo6ku', '::1', 1502966472, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936363437323b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('gkretru6438vqc9eqr5h15sda079jpg8', '::1', 1502968244, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936383234343b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('i9ited8mp0gb3a4buqip69fepoig37rc', '::1', 1502965625, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936353632353b),
+('r4l9uinavv7r62r922n80ahp3s6fvfcg', '::1', 1502968562, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936383536323b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('rv9if6kgkndbck64a9004hv0eu855guc', '::1', 1502968725, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936383536323b636c69656e74657c4e3b6c6f6761646f7c623a303b),
+('tgldtg342lbmqf2qf4vbltedujvvkjrn', '::1', 1502964890, 0x5f5f63695f6c6173745f726567656e65726174657c693a313530323936343839303b);
 
 -- --------------------------------------------------------
 
@@ -68,6 +98,14 @@ CREATE TABLE `clientes` (
   `cadastrado_em` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `sobrenome`, `rg`, `cpf`, `data_nascimento`, `sexo`, `rua`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `telefone`, `celular`, `email`, `senha`, `status`, `cadastrado_em`) VALUES
+(1, 'Elyas', 'Correa Nogueira', 'MG21006635', '117.290.346-83', '1969-12-31 00:00:00', 'M', 'Rua Noruega', '8', 'Jardim Andere', 'Varginha', 'Minas Gerais', '37006340', '35997441124', '35997441124', 'elyasnog2@hotmail.com', '123456', 0, '2017-08-17 10:17:07'),
+(8, 'Paulo', 'Guina', 'MG11111111', '111.111.111-11', '1970-09-08 00:00:00', 'M', 'Rua Noruega', '8', 'Jardim Andere', 'Varginha', 'MG', '37006-340', '(35)3321.1111', '(35)98784.5156', 'elyasnog@gmail.com', 'elyaselyas123', 0, '2017-08-17 11:18:43');
+
 -- --------------------------------------------------------
 
 --
@@ -93,11 +131,10 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `codigo`, `titulo`, `descricao`, `preco`, `largura_caixa_mm`, `altura_caixa_mm`, `comprimento_caixa_mm`, `peso_gramas`, `data_adicionado`, `ativo`) VALUES
-(1, '1', 'Chocolate Meio Amargo', 'Chocolate Meio Amargo 200g', '1.20', 1, 1, 1, 1, '2017-08-09 15:14:42', 1),
-(2, '2', 'Espaguete Santa Amália', 'Espaguete Santa Amália 1kg', '3.40', 1, 1, 1, 1, '2017-08-09 15:16:10', 1),
-(3, '3', 'Pai Nosso Santa Amália', 'Pai Nosso Santa Amália 1kg', '5.10', 1, 1, 1, 1, '2017-08-09 15:16:10', 1),
-(4, '4', 'Hamburgão do Greib', 'Hamburgão do Greib 100g', '2.50', 1, 1, 1, 1, '2017-08-09 15:17:32', 1),
-(5, '5', 'Chocolate Orgânico', 'Chocolate Orgânico 85g', '1.20', 1, 1, 1, 1, '2017-08-09 15:17:32', 1);
+(1, '14252', 'chocolate amargo', 'amargoo', '42.00', 42, 63, 13, 62, '2017-08-10 10:18:17', 1),
+(2, '4272', 'Hamburgão do Greib', 'Hamburgão do Greib 50g', '23.00', 54, 25, 12, 53, '2017-08-10 10:20:51', 1),
+(3, '2613', 'pastel', 'pastel de queijo', '51.00', 51, 62, 12, 62, '2017-08-10 10:20:51', 1),
+(4, '352512', 'Chocolate ao Leite', 'chocolate não amargo', '26.00', 47, 22, 23, 72, '2017-08-16 14:13:05', 1);
 
 -- --------------------------------------------------------
 
@@ -115,11 +152,10 @@ CREATE TABLE `produtos_categoria` (
 --
 
 INSERT INTO `produtos_categoria` (`produto`, `categoria`) VALUES
-(1, 1),
-(5, 1),
-(2, 2),
-(3, 2),
-(4, 2);
+(2, 1),
+(1, 2),
+(4, 2),
+(3, 3);
 
 --
 -- Indexes for dumped tables
@@ -130,6 +166,13 @@ INSERT INTO `produtos_categoria` (`produto`, `categoria`) VALUES
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
 
 --
 -- Indexes for table `clientes`
@@ -161,17 +204,17 @@ ALTER TABLE `produtos_categoria`
 -- AUTO_INCREMENT for table `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --

c0fe9ad5d9de3ccd6ca0a3adc9ee76e3a08dff82
Elyas Correa Nogueira <elyasnog2@hotmail.com>
2017-08-22 19:59:54 -0300

    pdf 7

--- application/views/html-header.php
+++ application/views/html-header.php
@@ -7,7 +7,7 @@
             echo meta(array('name' => 'viewport', 'content' => 'width=device-width,initial-scale=1.0')) .
             link_tag('assets/css/bootstrap.min.css') .
             link_tag('assets/css/bootstrap-responsive.min.css') .
-            link_tag('assets/estilos.css');
+            link_tag('assets/css/estilos.css');
         ?>
         <script src="<?php echo base_url('assets/js/jquery-3.1.1.min.js');?>">
         </script>

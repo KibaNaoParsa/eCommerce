f9ba92ee4173526b8e62cc5f49c3cf3876a55bc0
KibaNaoParsa <elyasnog2@hotmail.com>
2017-08-30 10:36:32 -0300

    pdf 10 (1.6 incompleto)

--- application/views/carrinho.php
+++ application/views/carrinho.php
@@ -26,10 +26,10 @@
         "</div>";
         $contador++;
     }
-    
-    
-    echo br() . "<div class='row-fluid'>" . 
-    "<div class='span3'>" . 
+
+
+    echo br() . "<div class='row-fluid'>" .
+    "<div class='span3'>" .
         form_submit("btnAtualizar", "Atualizar") .
     "</div>" .
     "<div class='span6'>" .
@@ -41,23 +41,23 @@
     "</div>" .
     "</div>" .
     form_close();
-      
-	if($frete) {
-		
-		echo "<div class='row-fluid'>".
-			"<div class=''span9></div>".
-			"<div class='span1 texto-direita'>Frete: </div>" .
-			"<div class='span2 texto-direita'>" . $reais($frete) . "</div>" .
-			"</div><div class='row-fluid'>" .
-			"<div class='span8'>" . anchor(base_url("pagar-e-finalizar") , "Pagar e finalizar compra") . "</div>" .
-			"<div class='span2 texto-direita'>Total da compra: </div>" .
-			"<div class='span2 texto-direita'>" . reais($this->cart->total() + $frete) . 
-			"</div>" . "</div>";
-	} else {
 
-		echo "<div class='row-fluid'>" . 
-			"<div class='span12 texto-direita'>" . "Efetue " . anchor(base_url('login'), 'Login') .
-			" para calcular o frete e finalizar a compra" . 
-			"</div>" . "</div>";
-	}
+    if($frete) {
+
+        echo "<div class='row-fluid'>".
+            "<div class=''span9></div>".
+            "<div class='span1 texto-direita'>Frete: </div>" .
+            "<div class='span2 texto-direita'>" . reais($frete) . "</div>" .
+            "</div><div class='row-fluid'>" .
+            "<div class='span8'>" . anchor(base_url("pagar-e-finalizar") , "Pagar e finalizar compra") . "</div>" .
+            "<div class='span2 texto-direita'>Total da compra: </div>" .
+            "<div class='span2 texto-direita'>" . reais($this->cart->total() + $frete) .
+            "</div>" . "</div>";
+    } else {
+
+        echo "<div class='row-fluid'>" .
+            "<div class='span12 texto-direita'>" . "Efetue " . anchor(base_url('login'), 'Login') .
+            " para calcular o frete e finalizar a compra" .
+            "</div>" . "</div>";
+    }
 

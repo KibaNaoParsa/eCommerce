0788ea33829941b1c35b6f2aa63513154370bff5
KibaNaoParsa <elyasnog2@hotmail.com>
2017-09-14 19:07:55 +0000

    Bonitificação da tela inicial

--- application/views/Administracao/inicial.php
+++ application/views/Administracao/inicial.php
@@ -5,14 +5,37 @@
         		nas tabelas de categorias, produtos ou frete.</p>
     </div>
     <div class="row-fluid">
-        <div align="left" id="conteudo" class="col-md-4">
-        <?php echo anchor(base_url("Administracao/v_cadastroCategoria"), "Cadastro de Categoria", 'class= "btn btn-primary"'); ?>
-        </div>
-        <div align="center" id="conteudo" class="col-md-4">
-        <?php echo anchor(base_url("Administracao/v_cadastroProduto"), "Cadastro de Produto", 'class= "btn btn-primary"'); ?>
-        </div>
-        <div align="right" id="conteudo" class="col-md-4">
+		<div align="left" id="conteudo" class="col-md-4">
+ 			<div class="dropdown">
+  				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Categorias <span class="caret"></span>
+  				</button>
+	  			<ul class="dropdown-menu">
+					<?php echo anchor(base_url("Administracao/v_cadastroCategoria"), "Cadastro de Categoria").br();
+							echo " <li class='divider'></li> ";
+	 	   	  			echo anchor(base_url("Administracao/v_edicaoCategoria"), "Edição de Categoria");
+							echo " <li class='divider'></li> ";
+	 	   	  			echo anchor(base_url("Administracao/v_exclusaoCategoria"), "Exclusão de Categoria"); ?>
+
+  				</ul>
+			</div> 
+    	</div>
+		<div align="center" id="conteudo" class="col-md-4">
+ 			<div class="dropdown">
+  				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Produtos <span class="caret"></span>
+  				</button>
+	  			<ul class="dropdown-menu">
+					<?php echo anchor(base_url("Administracao/v_cadastroProduto"), "Cadastro de Produto").br();
+							echo " <li class='divider'></li> ";
+	 	   	  			echo anchor(base_url("Administracao/v_edicaoProduto"), "Edição de Produto");
+							echo " <li class='divider'></li> ";
+	 	   	  			echo anchor(base_url("Administracao/v_exclusaoProduto"), "Exclusão de Produto"); ?>
+
+  				</ul>
+			</div> 
+    	</div>
+      <div align="right" id="conteudo" class="col-md-4">
         <?php echo anchor(base_url("Administracao/v_tabelaFrete"), "Tabela de Frete", 'class= "btn btn-primary"'); ?>
-        </div>
-    </div>
+      </div>    
+   </div>
+
 </div>
